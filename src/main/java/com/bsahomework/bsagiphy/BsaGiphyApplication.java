a46277882bb29590480ff40fd322b4aff0be6f73
Maxim <skorokhodov.maxim.444@gmail.com>
2021-06-21 17:24:45 +0300

    init repo

---
+++ src/main/java/com/bsahomework/bsagiphy/BsaGiphyApplication.java
@@ -0,0 +1,13 @@
+package com.bsahomework.bsagiphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsaGiphyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsaGiphyApplication.class, args);
+    }
+
+}
