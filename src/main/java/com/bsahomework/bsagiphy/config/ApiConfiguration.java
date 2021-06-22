84d2ac94d450740f0027ef10dc34472b05398220
Maxim <skorokhodov.maxim.444@gmail.com>
2021-06-22 21:04:06 +0300

    create api client

---
+++ src/main/java/com/bsahomework/bsagiphy/config/ApiConfiguration.java
@@ -0,0 +1,18 @@
+package com.bsahomework.bsagiphy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.net.http.HttpClient;
+
+@Configuration
+public class ApiConfiguration {
+
+    @Bean
+    @Scope(value = "prototype")
+    public HttpClient httpClient() {
+        return HttpClient.newHttpClient();
+    }
+
+}
